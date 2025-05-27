f48f06d04a7117355ec37ef48e0b553ae979529d
ahmttdmr <141131991+ahmttdmr@users.noreply.github.com>
2025-05-27 03:08:41 +0300

    Delete src/main directory

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.satranc2 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires org.kordamp.bootstrapfx.core;
-
-    opens com.example.satranc2 to javafx.fxml;
-    exports com.example.satranc2;
-}
